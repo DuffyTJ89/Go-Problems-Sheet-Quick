9f1c79d66f9b2eab12b39b9b9db27f37dbef67a7
DuffyTJ89 <tomduffy89@gmail.com>
2017-09-21 18:47:11 +0100

    added problem 01

---
+++ 01-HelloWorld.go
@@ -0,0 +1,20 @@
+//Author : Thomas Duffy
+//Date : 21/9/17
+
+//Adapted from : https://tour.golang.org/welcome/1
+
+
+package main
+
+/*
+Package fmt implements formatted I/O with functions analogous to C's printf and scanf. 
+The format 'verbs' are derived from C's but are simpler.
+*/
+
+import "fmt"
+
+func main() {
+	fmt.Println("Kon'nichiwa sekai")
+	fmt.Println("こんにちは世界")
+}//end function
+

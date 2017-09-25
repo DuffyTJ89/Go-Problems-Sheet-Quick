b1b6dc8f1efd6233f63ea70ea5afe30b1e3b8da0
DuffyTJ89 <tomduffy89@gmail.com>
2017-09-25 13:33:48 +0100

    Added Q6

---
+++ 06-LargestSmallest.go
@@ -0,0 +1,30 @@
+//Author : Thomas Duffy
+//Date : 25/9/17
+
+//Adapted from : https://play.golang.org/p/vhHmjhOMEo
+
+package main
+
+import "fmt"
+
+func main() { //function to create a list of numbers
+	x := []int{
+		48, 96, 86, 68,
+		57, 82, 63, 70,
+		37, 34, 83, 27,
+		19, 97, 9, 17,
+	}
+
+	smallest, biggest := x[0], x[0] //set largest and smallest to 0
+	for _, v := range x { //loop through the list
+		if v > biggest { //if the number is bigger than the last sorted largest overwrite it 
+			biggest = v
+		}
+		if v < smallest { //if the number is smaller than the last sorted smallest overwrite it 
+			smallest = v
+		}
+	}
+	//print out largest and smallest 
+	fmt.Println("The biggest number is ", biggest)
+	fmt.Println("The smallest number is ", smallest)
+}

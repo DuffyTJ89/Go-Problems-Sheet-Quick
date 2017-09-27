61128871dc9198fe3fff68aedb81e13cff9c2d40
DuffyTJ89 <tomduffy89@gmail.com>
2017-09-27 11:45:39 +0100

    finished Q7

--- 07-Palindrome.go
+++ 07-Palindrome.go
@@ -2,31 +2,31 @@
 //Date : 27/9/17
 
 //Adapted from : https://play.golang.org/p/5FUOzjBa-o
+//				 
 
 package main
 
 import "fmt"
 
-func isPalindrome(input string) bool { 
-	for i := 0; i < len(input)/2; i++ {
-		if input[i] != input[len(input)-i-1] {
+func isPalindrome(input string) bool { //function that takes a user inout which is passed from the main 
+	for i := 0; i < len(input)/2; i++ { //loop over the individual characters in the word for the length of the word. Divide it in 2 for check
+		if input[i] != input[len(input)-i-1] { //if there are any differences between the characters when the word is split in 2 then return false
 			return false
 		}
 	}
-	return true
+	return true //otherwise the test returns true
 }
 
 func main() {
 
-	var userInput
+	var userInput string
 	
-	fmt.Println("Palindrome check : ")
-    fmt.Scanf("%s", userInput)
-
-
-	fmt.Println(isPalindrome(userInput))
-	//fmt.Println(isPalindrome("bob"))
-}
+		fmt.Println("Palindrome check : ")
+		fmt.Scanf("%s ", &userInput)  //user input from console 
+		
+		fmt.Println(isPalindrome(userInput)) //call the function where the checking takes place and pss it the word  the user entered.
+	
+}//end main
 
 
 

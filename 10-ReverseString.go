f6f30780d48166b35c17081413da83c0e3d192ed
DuffyTJ89 <tomduffy89@gmail.com>
2017-10-04 13:21:53 +0100

    Added Q10

---
+++ 10-ReverseString.go
@@ -0,0 +1,24 @@
+//Author : Thomas Duffy
+//Date : 4/10/17
+
+//Adapted from : https://play.golang.org/p/Vq-TIHJCHE
+//				
+
+package main
+
+import "fmt"
+
+func Reverse(s string) string {
+	r := []rune(s) //rune maps character/string to their unicode codepoint.
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 { //loop over and swap characters around
+	r[i], r[j] = r[j], r[i]
+	}
+	return string(r) //return the string reversed
+}
+
+func main() { //some test words to reverse 
+	fmt.Println(Reverse("Hello, world"))
+	fmt.Println(Reverse("test"))
+	fmt.Println(Reverse("Thomas Duffy"))
+}
+
